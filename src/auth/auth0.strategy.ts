5efa5ab9ea448afca634ebf96e195751984e3332
Ben Elliot <benjiboi214@gmail.com>
2022-08-19 12:04:17 +1000

    Added offline_access scope for refresh token in token response

--- src/auth/auth0.strategy.ts
+++ src/auth/auth0.strategy.ts
@@ -10,7 +10,7 @@ export class Auth0Strategy extends PassportStrategy(Strategy) {
       clientID: process.env.AUTH0_CLIENT_ID,
       clientSecret: process.env.AUTH0_CLIENT_SECRET,
       callbackURL: process.env.AUTH0_CALLBACK_URL,
-      scope: 'openid email profile',
+      scope: 'openid email profile offline_access',
     });
   }
 
